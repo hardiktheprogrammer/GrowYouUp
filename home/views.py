13908a91521f893aeed2e44472f7c96af8a1398d
Aryan <aryanjainak@gmail.com>
2022-09-02 15:40:17 +0530

    final-commit

--- home/views.py
+++ home/views.py
@@ -6,7 +6,7 @@ from ratelimit.decorators import ratelimit
 from .models import Contact, Portfolio, Team, Testimonial
 
 
-@ratelimit(key='ip',rate='5/d',method=['POST'])
+@ratelimit(key='ip',rate='5/d',method=['POST'],block=True)
 def index(request):
     portfolio = Portfolio.objects.all().order_by('-time')[0:6]
     testimonial = Testimonial.objects.all().order_by('-published_at')[0:3]
@@ -17,8 +17,14 @@ def index(request):
         email =request.POST.get('email')
         phone = request.POST.get('phone')
         message = request.POST.get('message')
+        if len(phone) > 15:
+            messages.success(request,'Phone no. too long,please check again')
+            return HttpResponseRedirect(reverse('index:index'))
+        elif len(company_name) > 50:
+            messages.success(request,'Company Name too big')
+            return HttpResponseRedirect(reverse('index:index'))
         entry = Contact(name=name,company_name=company_name,email=email,phone=phone,message=message)
         entry.save()
-        messages.success(request,'Thanks will get back to u ASAP!')
+        messages.success(request,'Thanks for contacting,our team will connect with you soon!')
         return HttpResponseRedirect(reverse('index:index'))
     return render(request,'index/index.html',{'portfolio':portfolio,'testimonial':testimonial,'team':team})
